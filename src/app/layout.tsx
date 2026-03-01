afb052c1d97cbbb63bfb5540e74f7c13dcb42e55
T-H-Badhon <thbadhons@gmail.com>
2026-03-01 08:11:48 -0500

    cc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import { Toaster } from 'sonner';
@@ -39,6 +40,7 @@ export default function RootLayout({
                         className="fixed"
                     />
                 </div>
+                <script src="http://localhost:5002/tooltip-embed.js" data-api-key="dfddb5c29e78f6aef3f41badf74bae19e499172cf7c16f7e2b39dd1151e4cee4" data-base-url="http://localhost:5002/api"></script>
             </body>
         </html>
     );
